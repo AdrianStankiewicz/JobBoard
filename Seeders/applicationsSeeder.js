ebbbc6fef5c069b07bc015d596f72faa60a5891f
Adrian Stankiewicz <AdrianStankiewiczIT@gmail.com>
2023-05-05 18:05:12 +0200

    removed useless whitespace

--- Seeders/applicationsSeeder.js
+++ Seeders/applicationsSeeder.js
@@ -1,79 +1,79 @@
 db.applications.insertMany([
     {
-            offerId: ObjectId("64542d58d016389822eaca68"),
-            name: "Krzysztof Kowalczyk",
-            email: "kkowalczyk@gmail.com",
-            about: "GitHub: https://github.com/kkowalczyk\nGitLab: https://gitlab.com/kkowalczyk\nLinkedIn: https://linkedin.com/in/kkowalczyk",
-            cv: "D:\cv\1.pdf"
-        },
-        {
-            offerId: ObjectId("64542d58d016389822eaca69"),
-            name: "Krzysztof Kowalczyk",
-            email: "kkowalczyk@gmail.com",
-            about: "GitHub: https://github.com/kkowalczyk\nGitLab: https://gitlab.com/kkowalczyk\nLinkedIn: https://linkedin.com/in/kkowalczyk",
-            cv: "D:\cv\2.pdf"
-        },
-        {
-            offerId: ObjectId("64542d58d016389822eaca68"),
-            name: "Tomasz Majewski",
-            email: "tmajewski@gmail.com",
-            about: "GitLab: https://gitlab.com/tmajewski\nLinkedIn: https://linkedin.com/in/tmajewski",
-            cv: "D:\cv\3.pdf"
-        },
-        {
-            offerId: ObjectId("64542d58d016389822eaca69"),
-            name: "Tomasz Majewski",
-            email: "tmajewski@gmail.com",
-            about: "GitLab: https://gitlab.com/tmajewski\nLinkedIn: https://linkedin.com/in/tmajewski",
-            cv: "D:\cv\4.pdf"
-        },
-        {
-            offerId: ObjectId("64542d58d016389822eaca6a"),
-            name: "Krzysztof Kowalczyk",
-            email: "kkowalczyk@gmail.com",
-            about: "GitHub: https://github.com/kkowalczyk\nGitLab: https://gitlab.com/kkowalczyk\nLinkedIn: https://linkedin.com/in/kkowalczyk",
-            cv: "D:\cv\5.pdf"
-        },
-        {
-            offerId: ObjectId("64542d58d016389822eaca68"),
-            name: "Anna Nowak",
-            email: "anowak@gmail.com",
-            about: "LinkedIn: https://linkedin.com/in/anowak",
-            cv: "D:\cv\6.pdf"
-        },
-        {
-            offerId: ObjectId("64542d58d016389822eaca6f"),
-            name: "Jan Kowalski",
-            email: "jkowalski@gmail.com",
-            about: "GitHub: https://github.com/jkowalski\nGitLab: https://gitlab.com/jkowalski\nLinkedIn: https://linkedinin/in/jkowalski",
-            cv: "D:\cv\7.pdf"
-        },
-        {
-            offerId: ObjectId("64542d58d016389822eaca6f"),
-            name: "Krzysztof Kowalczyk",
-            email: "kkowalczyk@gmail.com",
-            about: "GitHub: https://github.com/kkowalczyk\nGitLab: https://gitlab.com/kkowalczyk\nLinkedIn: https://linkedin.com/in/kkowalczyk",
-            cv: "D:\cv\8.pdf"
-        },
-        {
-            offerId: ObjectId("64542d58d016389822eaca6e"),
-            name: "Krzysztof Kowalczyk",
-            email: "kkowalczyk@gmail.com",
-            about: "GitHub: https://github.com/kkowalczyk\nGitLab: https://gitlab.com/kkowalczyk\nLinkedIn: https://linkedin.com/in/kkowalczyk",
-            cv: "D:\cv\9.pdf"
-        },
-        {
-            offerId: ObjectId("64542d58d016389822eaca6d"),
-            name: "Magdalena Wojciechowska",
-            email: "mwojciechowska@gmail.com",
-            about: "GitHub: https://github.com/mwojciechowska\nGitLab: https://gitlab.com/mwojciechowska\nLinkedIn: https://linkedin.com/in/mwojciechowska",
-            cv: "D:\cv\10.pdf"
-        },
-        {
-            offerId: ObjectId("64542d58d016389822eaca6c"),
-            name: "Tomasz Majewski",
-            email: "tmajewski@gmail.com",
-            about: "",
-            cv: "D:\cv\11.pdf"
-        }          
+        offerId: ObjectId("64542d58d016389822eaca68"),
+        name: "Krzysztof Kowalczyk",
+        email: "kkowalczyk@gmail.com",
+        about: "GitHub: https://github.com/kkowalczyk\nGitLab: https://gitlab.com/kkowalczyk\nLinkedIn: https://linkedin.com/in/kkowalczyk",
+        cv: "D:\cv\1.pdf"
+    },
+    {
+        offerId: ObjectId("64542d58d016389822eaca69"),
+        name: "Krzysztof Kowalczyk",
+        email: "kkowalczyk@gmail.com",
+        about: "GitHub: https://github.com/kkowalczyk\nGitLab: https://gitlab.com/kkowalczyk\nLinkedIn: https://linkedin.com/in/kkowalczyk",
+        cv: "D:\cv\2.pdf"
+    },
+    {
+        offerId: ObjectId("64542d58d016389822eaca68"),
+        name: "Tomasz Majewski",
+        email: "tmajewski@gmail.com",
+        about: "GitLab: https://gitlab.com/tmajewski\nLinkedIn: https://linkedin.com/in/tmajewski",
+        cv: "D:\cv\3.pdf"
+    },
+    {
+        offerId: ObjectId("64542d58d016389822eaca69"),
+        name: "Tomasz Majewski",
+        email: "tmajewski@gmail.com",
+        about: "GitLab: https://gitlab.com/tmajewski\nLinkedIn: https://linkedin.com/in/tmajewski",
+        cv: "D:\cv\4.pdf"
+    },
+    {
+        offerId: ObjectId("64542d58d016389822eaca6a"),
+        name: "Krzysztof Kowalczyk",
+        email: "kkowalczyk@gmail.com",
+        about: "GitHub: https://github.com/kkowalczyk\nGitLab: https://gitlab.com/kkowalczyk\nLinkedIn: https://linkedin.com/in/kkowalczyk",
+        cv: "D:\cv\5.pdf"
+    },
+    {
+        offerId: ObjectId("64542d58d016389822eaca68"),
+        name: "Anna Nowak",
+        email: "anowak@gmail.com",
+        about: "LinkedIn: https://linkedin.com/in/anowak",
+        cv: "D:\cv\6.pdf"
+    },
+    {
+        offerId: ObjectId("64542d58d016389822eaca6f"),
+        name: "Jan Kowalski",
+        email: "jkowalski@gmail.com",
+        about: "GitHub: https://github.com/jkowalski\nGitLab: https://gitlab.com/jkowalski\nLinkedIn: https://linkedinin/in/jkowalski",
+        cv: "D:\cv\7.pdf"
+    },
+    {
+        offerId: ObjectId("64542d58d016389822eaca6f"),
+        name: "Krzysztof Kowalczyk",
+        email: "kkowalczyk@gmail.com",
+        about: "GitHub: https://github.com/kkowalczyk\nGitLab: https://gitlab.com/kkowalczyk\nLinkedIn: https://linkedin.com/in/kkowalczyk",
+        cv: "D:\cv\8.pdf"
+    },
+    {
+        offerId: ObjectId("64542d58d016389822eaca6e"),
+        name: "Krzysztof Kowalczyk",
+        email: "kkowalczyk@gmail.com",
+        about: "GitHub: https://github.com/kkowalczyk\nGitLab: https://gitlab.com/kkowalczyk\nLinkedIn: https://linkedin.com/in/kkowalczyk",
+        cv: "D:\cv\9.pdf"
+    },
+    {
+        offerId: ObjectId("64542d58d016389822eaca6d"),
+        name: "Magdalena Wojciechowska",
+        email: "mwojciechowska@gmail.com",
+        about: "GitHub: https://github.com/mwojciechowska\nGitLab: https://gitlab.com/mwojciechowska\nLinkedIn: https://linkedin.com/in/mwojciechowska",
+        cv: "D:\cv\10.pdf"
+    },
+    {
+        offerId: ObjectId("64542d58d016389822eaca6c"),
+        name: "Tomasz Majewski",
+        email: "tmajewski@gmail.com",
+        about: "",
+        cv: "D:\cv\11.pdf"
+    }
 ]);
