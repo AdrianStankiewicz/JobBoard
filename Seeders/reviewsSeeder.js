7c3fc25c4fdf82d0577be1df51863e53966dc8c1
Adrian Stankiewicz <AdrianStankiewiczIT@gmail.com>
2023-05-12 20:10:29 +0200

    changed companyId from string to ObjectId()

--- Seeders/reviewsSeeder.js
+++ Seeders/reviewsSeeder.js
@@ -1,266 +1,266 @@
 db.reviews.insertMany([
     {
         company: "Microsoft",
-        companyId: "64524ed062f914089ff73838",
+        companyId: ObjectId("64524ed062f914089ff73838"),
         title: "Fantastic company, supportive culture, recommended",
         review: "Microsoft is a fantastic company with a great work culture and supportive management. Highly recommend",
         score: 5
     },
     {
         company: "Google",
-        companyId: "64524ed062f914089ff7383a",
+        companyId: ObjectId("64524ed062f914089ff7383a"),
         title: "Innovative, boundary-pushing technology",
         review: "I've been working at Google for 5 years now and I couldn't be happier. The company is constantly innovating and pushing the boundaries of technology",
         score: 5
     },
     {
         company: "Apple",
-        companyId: "64524ed062f914089ff73839",
+        companyId: ObjectId("64524ed062f914089ff73839"),
         title: "Design-focused, creative atmosphere",
         review: "Apple is a great company with a strong focus on design and customer experience. The atmosphere is very creative and enjoyable to work in",
         score: 5
     },
     {
         company: "Cisco Systems",
-        companyId: "64524ed062f914089ff7383d",
+        companyId: ObjectId("64524ed062f914089ff7383d"),
         title: "Amazing place, fantastic team, pushing boundaries",
         review: "Cisco is an amazing place to work with a fantastic team. The company is always pushing the boundaries of what's possible",
         score: 5
     },
     {
         company: "HP Inc.",
-        companyId: "64524ed062f914089ff7383e",
+        companyId: ObjectId("64524ed062f914089ff7383e"),
         title: "Changing the world, talented team, exciting culture",
         review: "HP is a company that is truly changing the world. The work culture is fast-paced and exciting, and the team is full of talented individuals",
         score: 5
     },
     {
         company: "IBM",
-        companyId: "64524ed062f914089ff7383b",
+        companyId: ObjectId("64524ed062f914089ff7383b"),
         title: "Innovative, supportive culture, excellent management",
         review: "IBM is a great company with a long history of innovation. The work culture is very supportive and the management is excellent",
         score: 5
     },
     {
         company: "Oracle",
-        companyId: "64524ed062f914089ff7383c",
+        companyId: ObjectId("64524ed062f914089ff7383c"),
         title: "Technology-focused, fast-paced, rewarding culture",
         review: "Oracle is a great company with a strong focus on technology. The work culture is fast-paced and challenging, but also very rewarding",
         score: 5
     },
     {
         company: "Intel Corporation",
-        companyId: "64524ed062f914089ff73840",
+        companyId: ObjectId("64524ed062f914089ff73840"),
         title: "Technology-focused, innovative, fast-paced culture, supportive",
         review: "Intel is a great company with a strong focus on technology and innovation. The work culture is fast-paced and challenging, but also very supportive",
         score: 5
     },
     {
         company: "Acme Inc.",
-        companyId: "64524ed062f914089ff73847",
+        companyId: ObjectId("64524ed062f914089ff73847"),
         title: "Innovative, supportive, fast-paced culture",
         review: "Acme Inc. is a great company with a strong focus on innovation and customer satisfaction. The work culture is fast-paced and challenging, but also very supportive",
         score: 4
     },
     {
         company: "AwesomeTech",
-        companyId: "64524ed062f914089ff73848",
+        companyId: ObjectId("64524ed062f914089ff73848"),
         title: "Fantastic company, creative atmosphere, supportive management",
         review: "AwesomeTech is a fantastic company with a great team and supportive management. The atmosphere is very creative and enjoyable to work in",
         score: 5
     },
     {
         company: "WebSolutions",
-        companyId: "64524ed062f914089ff73849",
+        companyId: ObjectId("64524ed062f914089ff73849"),
         title: "Strong tech focus, talented team, exciting culture",
         review: "WebSolutions is a great company with a strong focus on technology and customer experience. The work culture is fast-paced and exciting, and the team is full of talented individuals",
         score: 5
     },
     {
         company: "CodeMasters",
-        companyId: "64524ed062f914089ff7384a",
+        companyId: ObjectId("64524ed062f914089ff7384a"),
         title: "Amazing place, fantastic team, pushing boundaries",
         review: "CodeMasters is an amazing place to work with a fantastic team. The company is always pushing the boundaries of what's possible in the world of technology",
         score: 5
     },
     {
         company: "Techworks",
-        companyId: "64524ed062f914089ff7384b",
+        companyId: ObjectId("64524ed062f914089ff7384b"),
         title: "Fast-paced, quality-focused, great for learners",
         review: "Techworks is a fast-paced and constantly growing company that prioritizes quality and reliability. It's great for those who like to work hard and learn quickly",
         score: 4
     },
     {
         company: "QualityFirst",
-        companyId: "645251b262f914089ff7384d",
+        companyId: ObjectId("645251b262f914089ff7384d"),
         title: "Innovative, supportive, excellent management",
         review: "QualityFirst is a company that is truly dedicated to delivering effective and innovative technology solutions. The work culture is supportive and the management is excellent",
         score: 5
     },
     {
         company: "Acme Inc.",
-        companyId: "64524ed062f914089ff73847",
+        companyId: ObjectId("64524ed062f914089ff73847"),
         title: "Expert team, innovative atmosphere, recommended",
         review: "The team at Acme Inc. is full of experts in their field and the atmosphere is very innovative. I highly recommend working here if you're looking for a challenging and exciting work environment",
         score: 4
     },
     {
         company: "AwesomeTech",
-        companyId: "64524ed062f914089ff73848",
+        companyId: ObjectId("64524ed062f914089ff73848"),
         title: "Creative, supportive management",
         review: "AwesomeTech is a great company with a strong focus on design and customer experience. The atmosphere is very creative and enjoyable to work in, and the management is very supportive",
         score: 5
     },
     {
         company: "WebSolutions",
-        companyId: "64524ed062f914089ff73849",
+        companyId: ObjectId("64524ed062f914089ff73849"),
         title: "Fast-paced, talented team, excellent management",
         review: "WebSolutions is a great company with a fast-paced work culture and a focus on technology. The team is full of talented individuals and the management is excellent",
         score: 5
     },
     {
         company: "CodeMasters",
-        companyId: "64524ed062f914089ff7384a",
+        companyId: ObjectId("64524ed062f914089ff7384a"),
         title: "Innovative, supportive, excellent management",
         review: "CodeMasters is a fantastic company with a strong focus on innovation and customer satisfaction. The work culture is fast-paced and challenging, but also very supportive and the management is excellent",
         score: 5
     },
     {
         company: "Acme Inc.",
-        companyId: "64524ed062f914089ff73847",
+        companyId: ObjectId("64524ed062f914089ff73847"),
         title: "Innovative but fast-paced culture",
         review: "Acme Inc. is a great company with a strong focus on innovation and customer satisfaction, but the work culture can be a bit fast-paced and stressful at times",
         score: 3
     },
     {
         company: "AwesomeTech",
-        companyId: "64524ed062f914089ff73848",
+        companyId: ObjectId("64524ed062f914089ff73848"),
         title: "",
         review: "AwesomeTech has a fantastic team and creative atmosphere, but the management could be more supportive at times",
         score: 4
     },
     {
         company: "WebSolutions",
-        companyId: "64524ed062f914089ff73849",
+        companyId: ObjectId("64524ed062f914089ff73849"),
         title: "Strong tech focus, overwhelming culture",
         review: "WebSolutions is a great company with a strong focus on technology, but the work culture can be a bit overwhelming at times",
         score: 4
     },
     {
         company: "CodeMasters",
-        companyId: "64524ed062f914089ff7384a",
+        companyId: ObjectId("64524ed062f914089ff7384a"),
         title: "Great team, needs better project management",
         review: "CodeMasters is an amazing place to work with a fantastic team, but the company could be more organized in terms of project management",
         score: 3
     },
     {
         company: "Techworks",
-        companyId: "64524ed062f914089ff7384b",
+        companyId: ObjectId("64524ed062f914089ff7384b"),
         title: "Innovative but needs better work-life balance",
         review: "Techworks is a fast-paced and constantly growing company that prioritizes quality and reliability, but the workload can be quite heavy at times",
         score: 4
     },
     {
         company: "QualityFirst",
-        companyId: "645251b262f914089ff7384d",
+        companyId: ObjectId("645251b262f914089ff7384d"),
         title: "",
         review: "QualityFirst is a company that is truly dedicated to delivering effective and innovative technology solutions, but the work-life balance could be better",
         score: 3
     },
     {
         company: "Acme Inc.",
-        companyId: "64524ed062f914089ff73847",
+        companyId: ObjectId("64524ed062f914089ff73847"),
         title: "Expert team, needs more employee focus",
         review: "The team at Acme Inc. is full of experts in their field and the atmosphere is very innovative, but the company could focus more on employee satisfaction",
         score: 2
     },
     {
         company: "AwesomeTech",
-        companyId: "64524ed062f914089ff73848",
+        companyId: ObjectId("64524ed062f914089ff73848"),
         title: "Design-focused but not inclusive",
         review: "AwesomeTech is a great company with a strong focus on design and customer experience, but the company culture could be more inclusive",
         score: 4
     },
     {
         company: "WebSolutions",
-        companyId: "64524ed062f914089ff73849",
+        companyId: ObjectId("64524ed062f914089ff73849"),
         title: "Fast-paced but lacking transparency",
         review: "WebSolutions is a great company with a fast-paced work culture and a focus on technology, but the management could be more transparent with employees",
         score: 3
     },
     {
         company: "CodeMasters",
-        companyId: "64524ed062f914089ff7384a",
+        companyId: ObjectId("64524ed062f914089ff7384a"),
         title: "Innovative but lacks employee support",
         review: "CodeMasters is a fantastic company with a strong focus on innovation and customer satisfaction, but the company could do more to support its employees",
         score: 3
     },
     {
         company: "Acme Inc",
-        companyId: "64524ed062f914089ff73847",
+        companyId: ObjectId("64524ed062f914089ff73847"),
         title: "Innovative but unreliable management",
         review: "Acme Inc. has a strong focus on innovation and customer satisfaction, but the management can be very unorganized and unreliable",
         score: 2
     },
     {
         company: "AwesomeTech",
-        companyId: "64524ed062f914089ff73848",
+        companyId: ObjectId("64524ed062f914089ff73848"),
         title: "Creative but toxic.",
         review: "AwesomeTech has a creative atmosphere, but the work culture can be very toxic and stressful",
         score: 1
     },
     {
         company: "WebSolutions",
-        companyId: "64524ed062f914089ff73849",
+        companyId: ObjectId("64524ed062f914089ff73849"),
         title: "Tech-focused, dismissive management",
         review: "WebSolutions is a great company with a strong focus on technology, but the management is very dismissive of employee concerns and complaints",
         score: 2
     },
     {
         company: "CodeMasters",
-        companyId: "64524ed062f914089ff7384a",
+        companyId: ObjectId("64524ed062f914089ff7384a"),
         title: "Great team, misses deadlines",
         review: "CodeMasters has a fantastic team, but the company is very disorganized and often misses deadlines",
         score: 2
     },
     {
         company: "Techworks",
-        companyId: "64524ed062f914089ff7384b",
+        companyId: ObjectId("64524ed062f914089ff7384b"),
         title: "Quality-focused but unresponsive",
         review: "Techworks is a fast-paced and constantly growing company that prioritizes quality and reliability, but the management is very unresponsive to employee needs",
         score: 3
     },
     {
         company: "QualityFirst",
-        companyId: "645251b262f914089ff7384d",
+        companyId: ObjectId("645251b262f914089ff7384d"),
         title: "Innovative but exclusive",
         review: "QualityFirst is a company that is dedicated to delivering effective and innovative technology solutions, but the company culture is very exclusive and not welcoming to diversity",
         score: 3
     },
     {
         company: "Acme Inc.",
-        companyId: "64524ed062f914089ff73847",
+        companyId: ObjectId("64524ed062f914089ff73847"),
         title: "Expert team, distant management",
         review: "The team at Acme Inc. is full of experts in their field and the atmosphere is very innovative, but the management is very unapproachable and distant",
         score: 4
     },
     {
         company: "AwesomeTech",
-        companyId: "64524ed062f914089ff73848",
+        companyId: ObjectId("64524ed062f914089ff73848"),
         title: "Design-focused and elitist",
         review: "AwesomeTech is a great company with a strong focus on design and customer experience, but the company culture can be very elitist and not inclusive",
         score: 2
     },
     {
         company: "WebSolutions",
-        companyId: "64524ed062f914089ff73849",
+        companyId: ObjectId("64524ed062f914089ff73849"),
         title: "Tech-focused but unsupportive",
         review: "WebSolutions is a great company with a fast-paced work culture and a focus on technology, but the company is very unsupportive of its employees and their well-being",
         score: 1
     },
     {
         company: "CodeMasters",
-        companyId: "64524ed062f914089ff7384a",
+        companyId: ObjectId("64524ed062f914089ff7384a"),
         title: "Innovative but disorganized",
         review: "CodeMasters is a fantastic company with a strong focus on innovation and customer satisfaction, but the company is very disorganized and lacks clear direction",
         score: 2
